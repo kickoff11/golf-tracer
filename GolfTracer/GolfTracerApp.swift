1216cc39916ada2c1076e3ac0ed52e410999dbfc
kickoff11 <irene.chaelin.lim@gmail.com>
2026-05-03 19:22:28 +0800

    Initial Xcode project (SwiftUI iOS app scaffold)

---
+++ GolfTracer/GolfTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfTracerApp.swift
+//  GolfTracer
+//
+//  Created by Vincent on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GolfTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
